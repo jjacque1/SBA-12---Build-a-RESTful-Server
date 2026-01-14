620fc3c939a36fd54c6e1eaac55b5dbabd2ceb6e
jjacque1 <jacksonjacque.jj@gmail.com>
2026-01-14 00:26:42 -0500

    night night

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,14 @@
 const omdbApiClient = require("../services/api-client");
 
+// GET /api/search?title=batman
 async function searchMovies(req, res) {
     const title = req.query.title;
 
     
+}
+
+
+// GET /api/movies/:id
+async function getMovieDetails(req, res) {
+
 }
